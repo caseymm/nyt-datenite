004b0cb293e75f66cfac84211ad2ef1736fbb8db
caseymm <caseymm@gmail.com>
2014-12-13 17:34:01 -0500

    search events

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,18 @@ datenite.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
 })
 
-datenite.controller('SearchCtrl', function($scope, $http){
-    $http.get('http://api.nytimes.com/svc/events/v2/listings.json?filters=category:Dance&date_range=2014-12-13%3A2014-12-31&api-key=3c9cba411d5c02c41b1d24aae1495dbe%3A8%3A70391628').success(function(data) {
-        $scope.results = data;
-      });
-})
+datenite.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.new_query = 'Dance';
+    $scope.submit = function() {
+        if($scope.new_query){
+            console.log(this.new_query)
+             $http.get('http://api.nytimes.com/svc/events/v2/listings.json?filters=category:'+this.new_query+'&date_range=2014-12-13%3A2014-12-31&api-key=3c9cba411d5c02c41b1d24aae1495dbe%3A8%3A70391628').success(function(data) {
+            $scope.results = data;
+          });
+        }
+      };
+
+}])
 datenite.controller('BrowseCtrl',  [
 // function() {
     console.log('browser')
