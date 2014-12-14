cb84634523f28da15017d5e6d5a2b575563db65d
Colleen McEnaney <colleenmcenaney@gmail.com>
2014-12-13 19:42:27 -0500

    added foursquare and google directions apis

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,42 @@ datenite.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
 })
 
+
+
+var venueClientID = "K2SCHC1JORKWLF3OCWGXY125Y2UXP4T2TQGCQUMLL0DCPHW5",
+	venueClientSecret = "LLTMPPQOXWFMY1HRI5PPJR5QY2RGOPT4Z5YQ1XFIXROG5UL1", 
+	venueLat = 40.7,
+	venueLng = -74,
+	venueKeywords = "bar",
+	venueSection = "food", //food, drinks, coffe, shops, arts, outdoors, sights, trending or specials
+	venueRadius = "600";
+
+var directionsKey = "AIzaSyCMGssX4Zzimz6Pt1HdKO00IxIFXEHriKs";
+//change origin and destination in URL to be latlong -> NOSPACES
+//origin is NYT venue, destination is venueLatvenueLong
+//could switch around
+
 datenite.controller('SearchCtrl', function($scope, $http){
     $http.get('http://api.nytimes.com/svc/events/v2/listings.json?filters=category:Dance&date_range=2014-12-13%3A2014-12-31&api-key=3c9cba411d5c02c41b1d24aae1495dbe%3A8%3A70391628').success(function(data) {
         $scope.results = data;
+        console.log(data);
       });
 })
+
+datenite.controller('VenuesCtrl', function($scope, $http){
+	$http.get('https://api.foursquare.com/v2/venues/search?client_id='+venueClientID+'&client_secret='+venueClientSecret+'&v=20130815%20&ll='+venueLat+','+venueLng+'%20&query='+venueKeywords+'%20&section='+venueSection+'%20&radius='+venueRadius).success(function(venuesData){
+		$scope.results = venuesData;
+		console.log(venuesData);
+	});
+})
+
+datenite.controller('DirectionsCtrl', function($scope, $http){
+	$http.get('https://maps.googleapis.com/maps/api/directions/json?origin=Brooklyn&destination=Queens&key='+directionsKey+'&departure_time=now&mode=transit').success(function(directionsData){
+		$scope.resultes = directionsData;
+		console.log(directionsData);
+	});
+})
+
 datenite.controller('BrowseCtrl',  [
 // function() {
     console.log('browser')
@@ -21,9 +52,8 @@ datenite.controller('BrowseCtrl',  [
 
 
 datenite.controller('AccordionCtrl', function($scope){
-	console.log("ghrehiiu")
-	$scope.groups = [];
-  for (var i=0; i<5; i++) {
+  $scope.groups = [];
+  for (var i=0; i<10; i++) {
     $scope.groups[i] = {
       name: i,
       items: []
@@ -47,4 +77,5 @@ datenite.controller('AccordionCtrl', function($scope){
   $scope.isGroupShown = function(group) {
     return $scope.shownGroup === group;
   };
+  
 })
